240a6bf904d288c2e2839e9aec0f37227934466c
unknown <RUK@LT-883524.statoil.net>
2015-09-20 21:23:11 +0200

    asas

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var port = process.env.PORT || 13337;
 
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World, here is something coming soon \n');
+    res.end('Hello World, here is something coming soon 1\n');
 }).listen(port);
