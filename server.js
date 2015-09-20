93709ece9a67a200caeb3e930056f7f0da5371df
unknown <RUK@LT-883524.statoil.net>
2015-09-20 19:12:13 +0200

    Small change

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var port = process.env.PORT || 13337;
 
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.end('Hello World, here is the food for you\n');
 }).listen(port);
